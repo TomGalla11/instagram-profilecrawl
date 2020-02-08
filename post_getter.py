8a83dfa08ef8782ba2e25b1e3578f3bdbb00de76
svmihar <svmihar@gmail.com>
2020-02-08 21:30:25 +0700

    :ire: removed mention, new no_hp method to regex :(

--- post_getter.py
+++ post_getter.py
@@ -5,28 +5,33 @@ from datetime import datetime
 from util.pencari_mention_hp import *
 
 
-def csv_jos(filename , outfile = ''):
+def csv_jos(filename, outfile=''):
     with open(filename) as f:
         lol = json.load(f)
 
     posts = []
-    for i,p in enumerate(lol['posts']):
-        hasil={'caption': p['caption'], 'likes':p['likes']['count'], 'sumber':lol['username'], 'url':p['url']}
+    for i, p in enumerate(lol['posts']):
+        hasil = {'caption': p['caption'], 'likes': p['likes']
+                 ['count'], 'sumber': lol['username'], 'url': p['url']}
         posts.append(hasil)
 
     df = pd.DataFrame(posts)
     df.drop_duplicates(inplace=True)
     df['telpon'] = df['caption'].apply(cek_hp)
-    df['mention'] = df['caption'].apply(cek_mention)
     return df
 
 
 def kumpulan_json():
     return [filename for filename in Path('./profiles').rglob('*.json')]
 
+
 def di_csv_kan():
     kumpulan_df = []
     for f in kumpulan_json():
         kumpulan_df.append(csv_jos(f))
     df = pd.concat(kumpulan_df)
-    df.to_csv(f'kumpulan_{datetime.now().strftime("%d_%m_%Y")}.csv', index=False)
+    df.to_csv(
+        f'kumpulan_{datetime.now().strftime("%d_%m_%Y")}_no_drop.csv', index=False)
+    df.dropna(inplace=True)
+    df.to_csv(
+        f'kumpulan_{datetime.now().strftime("%d_%m_%Y")}.csv', index=False)
--- util/pencari_mention_hp.py
+++ util/pencari_mention_hp.py
@@ -1,40 +1,12 @@
 from typing import List, Dict
+import re
 
 
 def cek_hp(caption):
-    hasil = []
-    for hp_str in caption.split():
-        if (len(hp_str)>10 and hp_str.isdigit()) or '08' in hp_str:
-            hasil.append(hp_str)
-        else:
-            hasil.append(None)
-    hasil = [h for h in hasil if h]
-    if hasil:
+    hasil = re.findall(
+        r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})', caption)
+
+    if len(hasil) > 0:
         return ' '.join(hasil)
     else:
         return None
-
-def cek_mention(caption):
-    hasil = []
-    for hp_str in caption.split():
-        if hp_str.startswith('@'):
-            hasil.append(hp_str[1:])
-        else:
-            hasil.append(None)
-    hasil = [h for h in hasil if h]
-    if hasil:
-        return ' '.join(hasil)
-    else:
-        return None
-
-
-
-
-if __name__=='__main__':
-    df = pd.read_excel('caption_ig.xlsx')
-    df.dropna(subset=['caption'], inplace=True)
-    df['telpon'] = df['caption'].apply(cek_hp)
-    df['mention'] = df['caption'].apply(cek_mention)
-    df.dropna(subset=['telpon'], inplace=True)
-    df.to_csv('test_caption_ig.csv', index=False)
-    df.to_csv('test_caption_ig.csv', index=False)
