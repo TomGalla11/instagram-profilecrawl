06470eddb78304e2d4f2f1fcd3889b37c2443503
TomGalla11 <67208362+TomGalla11@users.noreply.github.com>
2020-10-07 16:54:42 +0200

    Update extractor.py

--- util/extractor.py
+++ util/extractor.py
@@ -459,6 +459,7 @@ def extract_information(browser, username, limit_amount):
     try:
         user_link = "https://www.instagram.com/{}/".format(username)
         web_adress_navigator(browser, user_link)
+        print('Navigated')
     except PageNotFound404 as e:
         raise NoInstaProfilePageFound(e)
 
